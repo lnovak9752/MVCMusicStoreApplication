bf39d04d7402d04e39c3871d4e3747a428406616
Lucas Novak <lnovak9752@gmail.com>
2019-03-29 22:05:51 -0400

    Completed lab 10

---
+++ MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store/Index/4
+        public ActionResult Index(int id)
+        {
+            var albums = db.Albums.Where(a => a.GenreId.Equals(id));
+            return View(albums.ToList());
+        }
+
+        //GET: Store/Browse
+        public ActionResult Browse()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        //GET: Store/Details/3
+        public ActionResult Details(int id)
+        {
+            Album details = db.Albums.Find(id);
+            return View(details);
+        }
+        
+    }
+}
