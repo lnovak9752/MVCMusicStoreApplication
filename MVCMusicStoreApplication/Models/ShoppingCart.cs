8eae3b817d6d6c5c2fcda1ef3e034567ed4b3a85
Lucas Novak <lnovak9752@gmail.com>
2019-04-07 21:32:41 -0400

    Lab 11 completed

---
+++ MVCMusicStoreApplication/Models/ShoppingCart.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStoreApplication.Models
+{
+    public class ShoppingCart
+    {
+        public string ShoppingCartId;
+
+        public MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        public static ShoppingCart GetCart(HttpContextBase context)
+        {
+            ShoppingCart cart = new ShoppingCart();
+            cart.ShoppingCartId = cart.GetCartId(context);
+            return cart;
+        }
+
+        private string GetCartId(HttpContextBase context)
+        {
+            const string CartSessionId = "CartId";
+            string cartId;
+
+            if (context.Session[CartSessionId] == null)
+            {
+                cartId = Guid.NewGuid().ToString();
+
+                context.Session[CartSessionId] = cartId;
+            }
+            else
+            {
+                cartId = context.Session[CartSessionId].ToString();
+            }
+
+            return cartId;
+        }
+
+        public List<Cart> GetCartItems()
+        {
+            return db.Carts.Where(c => c.CartId == this.ShoppingCartId).ToList();
+        }
+
+        public decimal GetCartTotal()
+        {
+            decimal? total = (from cartItem in db.Carts
+                        where cartItem.CartId == this.ShoppingCartId
+                        select cartItem.AlbumSelected.Price * (int?)cartItem.Count).Sum();
+
+            //return total.HasValue ? total.Value : decimal.Zero;
+
+            return total ?? decimal.Zero;
+        }
+
+        public void AddToCart(int albumId)
+        {
+            //TODO: Verify that the album Id exists in the database
+            Cart cartItem = db.Carts.SingleOrDefault(c => c.CartId == this.ShoppingCartId && c.AlbumId == albumId);
+
+            if (cartItem == null)
+            {
+                cartItem = new Cart()
+                {
+                    CartId = this.ShoppingCartId,
+                    AlbumId = albumId,
+                    Count = 1,
+                    DateCreated = DateTime.Now
+                };
+
+                db.Carts.Add(cartItem);
+            }
+            else
+            {
+                cartItem.Count++;
+            }
+
+            db.SaveChanges();
+            
+        }
+
+        public int RemoveFromCart(int recordId)
+        {
+            Cart cartItem = db.Carts.SingleOrDefault(c => c.CartId == this.ShoppingCartId && c.RecordId == recordId);
+
+            if (cartItem == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            int newCount;
+            
+            if(cartItem.Count > 1)
+            {
+                cartItem.Count--;
+                newCount = cartItem.Count;
+            }
+            else
+            {
+                db.Carts.Remove(cartItem);
+                newCount = 0;
+            }
+            db.SaveChanges();
+            return newCount;
+        }
+    }
+}
---
+++ MVCMusicStoreApplication/Scripts/Apps/MusicScripts.js
@@ -0,0 +1,22 @@
+﻿
+$(function () { $("#album-list img").mouseover(function () { $(this).animate({ height: "+=25", width: " += 25" }).animate({ height: "-= 25", width: " -=25" }); }); });
+
+function searchFailed() {
+    $("#searchresults").html("Sorry, there was a problem with the search.");
+}
+
+$(function () {
+    $(".RemoveLink").click(function () {
+        var id = $(this).attr("data-id");
+
+        $.post("/ShoppingCart/RemoveFromCart", { "id": id }, function (data) {
+            $("#update-message").text(data.Message);
+            $("#cart-total").text(data.CartTotal);
+            $("#item-count-" + data.DeleteId).text(data.ItemCount);
+
+            if (data.ItemCount < 1) {
+                $("#record-" + data.DeleteId).fadeOut();
+            }
+        });
+    });
+});
